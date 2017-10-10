3eefd0685e587d70aabb981b3275113b0b5e84a7
Ahmet Celikbas <celikbas.ahmet@gmail.com>
2017-10-10 23:55:57 +0200

    Navbar with : - Authentication mock service to provide mock datas - Models

---
+++ src/app/services/authentication-mock.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Utilisateur } from '../models/Utilisateur';
+import {Contact} from '../models/Contact';
+import {Profil} from '../models/Profil';
+import {Groupe} from '../models/Groupe';
+
+@Injectable()
+export class AuthenticationMockService {
+
+  constructor() { }
+
+  getUserInfos(): Observable<Utilisateur> {
+    return new Observable(observer => {
+
+      const userSample: Utilisateur = new Utilisateur(
+        1,
+        'email@domain.com',
+        'MD5PASSWORD',
+        new Contact(
+          1,
+          'Nom',
+          'Prénom',
+          'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50',
+          '93 Rue du progrès',
+          '69000',
+          'LYON',
+          'email@domain.com',
+          new Profil(
+            1,
+            'SENIOR',
+            '#FFFF00',
+          ))
+      );
+
+      observer.next(userSample);
+      observer.complete();
+    });
+  }
+
+  getGroup(): Observable<Groupe> {
+    return new Observable(observer => {
+
+      const groupeSample: Groupe = new Groupe(
+        1,
+        'Nom du groupe',
+        '10/10/2017 - 00:00'
+      );
+
+      observer.next(groupeSample);
+      observer.complete();
+    });
+  }
+
+
+}
