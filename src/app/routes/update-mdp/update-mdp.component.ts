06178bb2c8b830d3495d2b4a0dad18c21710ebf6
eliole@hotmail.fr <Jesuis1superprogrammeur>
2017-10-17 17:26:14 +0200

    Composant réinitialisation Mdp

--- src/app/routes/update-mdp/update-mdp.component.ts
+++ src/app/routes/update-mdp/update-mdp.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl, Validators} from '@angular/forms';
 import {PASSWORD_VALIDATOR} from '../../validators/password.validator';
 import {HttpClient} from '@angular/common/http';
 import {ConfigService} from '../../services/config.service';
 import {AuthenticationService} from '../../services/authentication.service';
-import { Md5 } from 'ts-md5/dist/md5';
+import {Md5} from 'ts-md5/dist/md5';
 import {NavigatorService, PATH_LOGIN} from '../../services/navigator.service';
 
 @Component({
@@ -14,17 +14,20 @@ import {NavigatorService, PATH_LOGIN} from '../../services/navigator.service';
 })
 export class UpdateMdpComponent implements OnInit {
 
-  constructor(
-    private config: ConfigService,
-    private http: HttpClient,
-    public authService: AuthenticationService,
-    public nav: NavigatorService, ) { }
+  constructor(private config: ConfigService,
+              private http: HttpClient,
+              private authService: AuthenticationService,
+              private nav: NavigatorService, ) {
+  }
 
   password = new FormControl('', [Validators.required, Validators.pattern(PASSWORD_VALIDATOR)]);
   newPassword = new FormControl('', [Validators.required]);
-  hide = true;
+  hide = false;
+  token = '';
+
 
   ngOnInit() {
+    this.token = window.location.pathname.split(this.config.API_ROUTES.UPDATEPASSWORD)[1];
   }
 
   getPasswordErrorMessage() {
@@ -36,20 +39,25 @@ export class UpdateMdpComponent implements OnInit {
   }
 
   updatePassword() {
-    console.log('coucou');
+    this.http.get(`${this.config.API_BASE}${this.config.API_ROUTES.UTILISATEURS}` +
+      `${this.authService.getUserIdFromToken(this.token)}`).subscribe(
+      (utilisateur) => {
+        const newUserData = {
+          idUtilisateur: utilisateur['idUtilisateur'],
+          email: utilisateur['email'],
+          motDePasse: Md5.hashStr(this.password.value).toString(),
+          newIdContact: utilisateur['contact']['idContact'],
+        };
 
-    const postDataObjectToSend = {
-      idUtilisateur: 2,
-      email: 'celikbas.ahmet@gmail.com',
-      motDePasse: Md5.hashStr(this.password.value).toString(),
-      IdContact: 3,
-    };
-    this.http.put(`${this.config.API_BASE}${this.config.API_ROUTES.UTILISATEUR}`, postDataObjectToSend).
-    subscribe( (liste) => {
-      console.log('COUCOU2LERETOUR')
-      console.log(liste);
-      this.nav.router.navigate([PATH_LOGIN]);
+        this.http.put(`${this.config.API_BASE}${this.config.API_ROUTES.UTILISATEURS}`, newUserData).subscribe(
+          (response) => {
+
+            this.nav.router.navigate([PATH_LOGIN]);
+          },
+          (error) => console.log(error));
       },
-      (error) => console.log(error));
+      (error) => console.log(error)
+    );
+
   }
 }
