2130e1f2011ad69aad4f482411f42076de61a0d8
Damien Pickel <eliole@hotmail.fr>
2017-10-17 19:39:20 +0200

    Le mdp requière maintenant 6 caractères et non 7

--- src/app/validators/password.validator.ts
+++ src/app/validators/password.validator.ts
@@ -1,6 +1,6 @@
 import { FormGroup } from '@angular/forms';
 
-export const PASSWORD_VALIDATOR = '^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(\\S).{6,}$';
+export const PASSWORD_VALIDATOR = '^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(\\S).{5,}$';
 
 export const passwordMatchValidator = (g: FormGroup) => {
   return g.get('password').value === g.get('passwordConfirm').value ? null : {'mismatch': true};
